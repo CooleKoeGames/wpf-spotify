25ccf1ce1803322cde142640548084f528199cac
CooleKoeGames <133784369+CooleKoeGames@users.noreply.github.com>
2024-03-20 11:53:13 +0100

    files

---
+++ SpaansSpotifySearch/SpaansSpotifySearch/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace SpaansSpotifySearch
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
